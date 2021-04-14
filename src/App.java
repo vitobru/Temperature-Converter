52d1d9936f5e3bfbd55284b0d1b973246a5c19b5
Vito <jakobwilczynski211@gmail.com>
2021-04-13 23:05:00 -0700

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,7 +1,21 @@
 // Temp Converter
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Temperature Converter v0.0.1");
-        System.out.println("by Vitobru");
+        System.out.println("Temperature Converter v0.0.1 by Vitobru");
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter degrees Fahrenheit:");
+        float fahrenheit = input.nextFloat();
+        float result = fahrenheitCelsius(fahrenheit);
+        System.out.println(result);
+
+        input.close();
+    }
+
+    public static float fahrenheitCelsius(Float fahrenheit) {
+        float celsius = ((fahrenheit-32)*5/9);
+        return celsius;
     }
 }
