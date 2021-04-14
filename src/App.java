ff5db742522f6dee17de7257398dc4db5101ee07
Vito <jakobwilczynski211@gmail.com>
2021-04-13 21:14:35 -0700

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,7 @@
+// Temp Converter
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Temperature Converter v0.0.1");
+        System.out.println("by Vitobru");
+    }
+}
