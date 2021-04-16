4bf1c82efb9fba7605d9028e0e53e30c10d8fe47
Vito <jakobwilczynski211@gmail.com>
2021-04-15 17:28:39 -0700

    Update App.java

--- src/tempconvert/App.java
+++ src/tempconvert/App.java
@@ -1,23 +1,44 @@
 // Temp Converter
 package tempconvert;
 import java.util.Scanner;
-import tempconvert.ConvertUtil;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Temperature Converter v0.0.4 by Vitobru");
+
+        System.out.print("Temperature Converter v0.0.7 by Vitobru\n\nPick an operation:\n[1] Fahrenheit to Celsius\n[2] Celsius to Fahrenheit\n\n");
+        
+        String exitChoice = new String("");
         Scanner input = new Scanner(System.in);
+        
+        while (!exitChoice.equals("Exit")) {
+            
+            System.out.print("Enter operation: ");
+            int operation = input.nextInt();
+            
+            if (operation == 1) {
+                System.out.print("Degrees Fahrenheit: ");
+                float fahrenheit = input.nextFloat();
+                float celsius = ConvertUtil.celsiusFahrenheit(fahrenheit);
+                System.out.println(fahrenheit+" degrees Fahrenheit is "+celsius+" degrees in Celsius.");
+            }
 
-        System.out.print("Enter degrees Fahrenheit:");
-        float fahrenheit = input.nextFloat();
-        float result = fahrenheitCelsius(fahrenheit);
-        System.out.println(result);
+            else if (operation == 2) {
+                System.out.print("Degrees Celsius: ");
+                float celsius = input.nextFloat();
+                float fahrenheit = ConvertUtil.fahrenheitCelsius(celsius);
+                System.out.println(celsius+" degrees Celsius is "+fahrenheit+" degrees in Fahrenheit.");
+            }
 
+            else {
+                System.out.println("Please input a valid choice");
+            }
+            
+            String reset = new String(input.nextLine());
+            System.out.println("Type 'Exit' to quit the converter, or enter anything else to perform another operation.");
+            System.out.println("Response: ");
+            exitChoice = input.nextLine();
+        }
         input.close();
     }
-
-    public static float fahrenheitCelsius(Float fahrenheit) {
-        float celsius = ((fahrenheit-32)*5/9);
-        return celsius;
-    }
+    
 }
